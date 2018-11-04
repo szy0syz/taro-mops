d65998fdcd94d0de2669258972cef8ddc767e726
szy0syz <szy0syz@gmail.com>
2018-11-04 22:15:09 +0800

    Update 更新 order 页面布局

--- src/pages/orderAdd/index.js
+++ src/pages/orderAdd/index.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View, Button, Text, Image, Input } from '@tarojs/components';
+import { View, Button, Text, Image, Input, Picker } from '@tarojs/components';
 import { connect } from '@tarojs/redux';
 import { AtList, AtListItem, AtIcon, AtCheckbox, AtButton } from 'taro-ui'
 import './index.scss'
@@ -11,6 +11,16 @@ export default class Order extends Component {
   constructor() {
     super(...arguments);
     this.state = {
+      billData: {
+        billDate: '',
+        customer: '',
+        products: [],
+        staff: '',
+        storekeeper: '',
+        amountRec: 0,
+        paymentMethod: '',
+        billTags: []
+      },
       checkedList: ['list2'],
       checkboxOption: [{
         value: 'list1',
@@ -24,9 +34,44 @@ export default class Order extends Component {
       }, {
         value: 'list4',
         label: '已同步'
-      }]
+      }],
+      payTypes: [
+        {
+          key: '银行汇款',
+          value: 'v1'
+        },
+        {
+          key: '现金',
+          value: 'v2'
+        },
+        {
+          key: '微信',
+          value: 'v3'
+        },
+        {
+          key: '支付宝',
+          value: 'v4'
+        }
+      ],
+      payTypeChecked: {
+        key: '银行汇款',
+        value: 'v1'
+      },
+      storekeeperList: [
+        {
+          name: '李四四',
+          id: 'lss'
+        },
+        {
+          name: '王五五',
+          id: 'wmz'
+        }
+      ],
+      storekeeperChecked: {
+        name: '李四四',
+        id: 'lss'
+      }
     }
-
   }
 
   config = {
@@ -37,21 +82,32 @@ export default class Order extends Component {
   }
 
   handleChange(value) {
-    this.setState({
-      checkedList: value
-    })
+    let billData = Object.assign({}, this.state.billData, { billTags: value })
+    console.log(billData)
+    // this.setState({
+    //   billData
+    // })
+  }
+
+  handleBillDateChange(value) {
+    let billData = Object.assign({}, this.state.billData, { billTags: value })
+    console.log(billData)
   }
 
   render() {
     return (
       <View className='order-page'>
         <View className='order-wrapper'>
+          <Picker className='date-selector' mode='date' range={this.state.storekeeperList} rangeKey='name' onChange={this.handleBillDateChange}>
+            <View className='picker'>
+              {this.state.storekeeperChecked.name}
+              <AtIcon value='chevron-right' size='22' color='#999'></AtIcon>
+            </View>
+          </Picker>
           <AtList>
             <AtListItem
               className='custom-listItem'
               title='日期'
-              extraText='2018-11-05'
-              arrow='right'
               iconInfo={{
                 size: 28,
                 color: '#999',
@@ -77,12 +133,12 @@ export default class Order extends Component {
             <Button style='background-color: #1fb7a6;' className='custom-button' size='mini'>存为模板</Button>
           </View>
           <View>
-            <Text>选择货品(5.00)</Text>
+            <Text>选择货品(4)</Text>
             <Text>合计金额：￥2080.00</Text>
           </View>
           <View>
             <View className='order-item'>
-              <AtIcon value='add-circle' size='46' color='#00F'></AtIcon>
+              <Image className='m-img' src='http://cdn.jerryshi.com/picgo/20181104150040.png'></Image>
               <View>
                 <Text>敌杀死</Text>
                 <View className='order-cell'>
@@ -97,7 +153,7 @@ export default class Order extends Component {
               <AtIcon value='subtract-circle' size='30' color='#F00'></AtIcon>
             </View>
             <View className='order-item'>
-              <AtIcon value='add-circle' size='46' color='#00F'></AtIcon>
+              <Image className='m-img' src='http://cdn.jerryshi.com/picgo/20181104150040.png'></Image>
               <View>
                 <Text>敌百强</Text>
                 <View className='order-cell'>
@@ -112,9 +168,24 @@ export default class Order extends Component {
               <AtIcon value='subtract-circle' size='30' color='#F00'></AtIcon>
             </View>
             <View className='order-item'>
-              <AtIcon value='add-circle' size='46' color='#00F'></AtIcon>
+              <Image className='m-img' src='http://cdn.jerryshi.com/picgo/20181104150040.png'></Image>
               <View>
                 <Text>敌百万</Text>
+                <View className='order-cell'>
+                  <Text>单价：￥66.00</Text>
+                  <Text>数量：10.00公斤</Text>
+                </View>
+                <View className='order-cell'>
+                  <Text>规格：200g 水乳剂</Text>
+                  <Text>金额：￥660.00</Text>
+                </View>
+              </View>
+              <AtIcon value='subtract-circle' size='30' color='#F00'></AtIcon>
+            </View>
+            <View className='order-item'>
+              <Image className='m-img' src='http://cdn.jerryshi.com/picgo/20181104150040.png'></Image>
+              <View>
+                <Text>敌千万</Text>
                 <View className='order-cell'>
                   <Text>单价：￥66.00</Text>
                   <Text>数量：10.00公斤</Text>
@@ -131,8 +202,36 @@ export default class Order extends Component {
             <Image src='http://cdn.jerryshi.com/picgo/scanAdd.png'></Image>
             <Image src='http://cdn.jerryshi.com/picgo/plusAdd.png'></Image>
           </View>
+        </View>
+        <View className='order-wrapper order-footer'>
           <View>
-            票据影像
+            <Text>应收金额</Text>
+            <Input type='digit' placeholder='0.00' className='input-amount'></Input>
+          </View>
+          <View>
+            <Text>结算方式</Text>
+            <Picker mode='selector' range={this.state.payTypes} rangeKey='key' onChange={this.onChange}>
+              <View className='picker'>
+                {this.state.payTypeChecked.key}
+                <AtIcon value='chevron-right' size='22' color='#999'></AtIcon>
+              </View>
+            </Picker>
+          </View>
+          <View>
+            <Text>业务员</Text>
+            <Text>张三三</Text>
+          </View>
+          <View>
+            <Text>出库员</Text>
+            <Picker mode='selector' range={this.state.storekeeperList} rangeKey='name' onChange={this.onChange}>
+              <View className='picker'>
+                {this.state.storekeeperChecked.name}
+                <AtIcon value='chevron-right' size='22' color='#999'></AtIcon>
+              </View>
+            </Picker>
+          </View>
+          <View>
+            <Text>票据影像</Text>
             <View>
               <AtIcon value='camera' size='34' color='#fff'></AtIcon>
             </View>
