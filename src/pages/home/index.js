ede79b3eed5a9211b21a82a0a38b2b9c534027e6
szy0syz <szy0syz@gmail.com>
2018-11-26 23:41:36 +0800

    Update 精简首页样式

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View, Text } from '@tarojs/components';
+import { View, Text, Image } from '@tarojs/components';
 import { AtSearchBar, AtIcon } from 'taro-ui'
 import { connect } from '@tarojs/redux';
 import MySwiper from '../../components/MySwiper';
@@ -16,13 +16,6 @@ export default class Index extends Component {
   };
 
   componentDidMount = () => {
-    // this.props.dispatch({
-    //   type: 'home/load',
-    // });
-    // this.props.dispatch({
-    //   type: 'home/product',
-    // });
-
     // 设置衣袋小红点
     if (this.props.items.length > 0) {
       Taro.setTabBarBadge({
@@ -61,58 +54,16 @@ export default class Index extends Component {
     const { keyword } = this.props;
     return (
       <View className='home-page'>
-        <View className='upper-area'>
-          <AtSearchBar
-            placeholder='搜索商品名称/规格/条形码/助记码'
-            showActionButton
-            value={keyword}
-            onChange={this.onChange.bind(this)}
-          />
-          <MySwiper></MySwiper>
-          <View className='title-column'>
-            <View className='item-stats'>
-              <Text style='font-size: 30rpx;'>￥1688.00</Text>
-              <Text style='padding-top:6rpx;' className='column-lable'>本月销售</Text>
-            </View>
-            <View className='item-text'>
-              <View>
-                <AtIcon value='credit-card' size='26' color='#fff'></AtIcon>
-                <Text className='column-lable'>资金</Text>
-              </View>
-              <View>
-                <AtIcon value='calendar' size='25' color='#fff'></AtIcon>
-                <Text className='column-lable'>订单</Text>
-              </View>
-            </View>
-            <View className='item-stats'>
-              <Text style='font-size: 30rpx;'>￥711548.00</Text>
-              <Text style='padding-top:6rpx;' className='column-lable'>库存金额</Text>
-            </View>
-          </View>
-        </View>
+        <AtSearchBar
+          placeholder='搜索商品名称/规格/助记码/订单号'
+          showActionButton
+          value={keyword}
+          onChange={this.onChange.bind(this)}
+        />
+        <Image className='logo' src='http://cdn.jerryshi.com/picgo/20181126232346.png'></Image>
         <View>
-        </View>
-        <View className='grid-menu'>
-          <View>
-            <View className='menu-icon'>
-              <AtIcon value='user' size='26' color='#fff'></AtIcon>
-            </View>
-            <View className='menu-text'>客户管理</View>
-            <View className='menu-text' style='color: #2bb2a7'>￥12150.00</View>
-          </View>
-          <View>
-            <View className='menu-icon'>
-              <AtIcon value='bullet-list' size='26' color='#fff'></AtIcon>
-            </View>
-            <View className='menu-text'>商品查看</View>
-          </View>
-          <View>
-            <View className='menu-icon'>
-              <AtIcon value='money' size='26' color='#fff'></AtIcon>
-            </View>
-            <View className='menu-text'>供应商管理</View>
-            <View className='menu-text' style='color: #2bb2a7'>￥29880.00</View>
-          </View>
+          <Text className='cname'>云农农业科技 \n</Text>
+          <Text className='cname'>MOPS系统 </Text>
         </View>
       </View>
     )
